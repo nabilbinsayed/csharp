f438e67868647e6e4f6e76d3b7c368dee5c28e58
nabilbinsayed <nabilprometheus@gmail.com>
2025-06-09 21:45:59 +0600

    A lot of things

---
+++ TTT Game/TTT Game/Program.cs
@@ -0,0 +1,41 @@
+﻿using System; 
+namespace TTT
+{
+    public class Program
+    {
+        static bool[,] field = new bool[,]
+        {
+            { false, false, false },
+            { false, false, false },
+            { false, false, false }
+        };
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the game!");
+            PrintBoard();
+        }
+
+        public static void PrintBoard()
+        {
+            int[,] board;
+            board = new int[9, 9];
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = i + j + 1;
+                }
+            }
+
+            
+
+
+        }
+
+
+
+
+    }
+}
